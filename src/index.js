daa56e75b6694c58fedd6a4c78a49e9f368256ad
Amp <amp.tanawat@gmail.com>
2021-04-22 04:55:24 +0700

    Extract all pure functions from main

--- src/index.js
+++ src/index.js
@@ -13,35 +13,16 @@ const app = {
 async function main() {
   const cartId = getUrlQuery("cart_id", location.search);
   const cart = await fetchCart(HOST, cartId); // 🅰️ Action
-  const products = cart.products.map((p) => ({
-    id: p.id,
-    quantity: p.quantity,
-  }));
-  const productsVariantId = products.map((p) => p.id);
+  const productsVariantId = productsVariantIdOfCart(cart);
   const cartItems = await fetchCartItems(SALEOR_HOST, productsVariantId); // 🅰️ Action
   const items = await constructItems(cart, cartItems); // 🅰️ Action
-  const totalPriceOfCart = items
-    .map((p) => p.price.total)
-    .reduce((a, b) => a + b, 0);
-  const cartModel = {
-    id: cartId,
-    products: items,
-    subtotal: Number(totalPriceOfCart).toLocaleString(),
-    shippingPrice: Number(SHIPPING_PRICE_DEFAULT).toLocaleString(),
-    total: Number(totalPriceOfCart + SHIPPING_PRICE_DEFAULT).toLocaleString(),
-  };
-
-  const countries = countries_
-    .map((c) => ({ name: c.name, code: c.code }))
-    .sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
-
-  const toElm = {
-    cart: cartModel,
-    countries,
-  };
-
+  const cartModel = createCartModel(cartId, items, SHIPPING_PRICE_DEFAULT);
+  const countries = buildCountries(countries_);
   setTimeout(() => {
-    app.ports.pages_shipping_fetchReceived.send(toElm); // 🅰️ Action
+    app.ports.pages_shipping_fetchReceived.send({
+      cart: cartModel,
+      countries,
+    }); // 🅰️ Action
   }, 1500);
 }
 
@@ -60,11 +41,49 @@ export function getUrlQuery(name, locationSearch) {
 }
 
 // 💚 Calculation
-// Receive cartItems as an argument
 function constructItems(cartItems) {
-  // Move fetch cart items out
   const { edges } = cartItems.data.productVariants;
   return edges;
+}
+
+// 💚 Calculation
+function productsVariantIdOfCart(cart) {
+  const products = cart.products.map((p) => ({
+    id: p.id,
+    quantity: p.quantity,
+  }));
+  const productsVariantId = products.map((p) => p.id);
+  return productsVariantId;
+}
+
+// 💚 Calculation
+function cartTotalPrice(items) {
+  const totalPriceOfCart = items
+    .map((p) => p.price.total)
+    .reduce((a, b) => a + b, 0);
+  return totalPriceOfCart;
+}
+
+// 💚 Calculation
+function createCartModel(cartId, items, defaultShippingPrice) {
+  // Calculation can use another calculation!
+  const totalPrice = cartTotalPrice(items);
+  const cartModel = {
+    id: cartId,
+    products: items,
+    subtotal: Number(totalPrice).toLocaleString(),
+    shippingPrice: Number(defaultShippingPrice).toLocaleString(),
+    total: Number(totalPrice + defaultShippingPrice).toLocaleString(),
+  };
+  return cartModel;
+}
+
+// 💚 Calculation
+function buildCountries(data) {
+  const countries = data
+    .map((c) => ({ name: c.name, code: c.code }))
+    .sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
+  return countries;
 }
 
 // Faked network functions
